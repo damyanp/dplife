65059d6cce03cf022361c8a2d6009cc90bc9bc6d
Justin Bogner <mail@justinbogner.com>
2025-04-30 21:56:45 -0700

    Refactor dxc compiler invocations (#4)
    ---------
    
    Co-authored-by: Damyan Pepper <damyanp@microsoft.com>

--- d3dx12/src/build.rs
+++ d3dx12/src/build.rs
@@ -1,9 +1,7 @@
 use std::{
-    env::{self, var},
+    env::var,
     fs::copy,
-    io::{stderr, stdout, Write},
-    path::Path,
-    process::Command,
+    path::{Path, PathBuf},
 };
 
 pub fn copy_data_file(source_path: &str) {
@@ -22,50 +20,10 @@ pub fn copy_data_file(source_path: &str) {
     copy(source_path, dest).expect("Copy");
 }
 
-pub fn dxc_command<P: AsRef<Path>>(source_path: P) -> Command {
-    println!(
-        "!cargo::rerun-if-changed={}",
-        source_path.as_ref().to_str().unwrap()
-    );
-
+pub fn get_dxc_path() -> PathBuf {
     static DXC_PATH: &str = concat!(
         env!("OUT_DIR"),
         "/packages/Microsoft.Direct3D.DXC/build/native/bin/x64/dxc.exe"
     );
-
-    let mut command = Command::new(DXC_PATH);
-    command.arg(source_path.as_ref());
-
-    command
-}
-
-pub fn dxc_compile<P1: AsRef<Path>, P2: AsRef<Path>>(
-    source_path: P1,
-    dest_path: P2,
-    profile: &str,
-    entry_point: &str,
-) {
-    let dest_path = Path::new(&env::var_os("OUT_DIR").unwrap()).join(dest_path);
-
-    let result = dxc_command(source_path)
-        .args([
-            "-T",
-            profile,
-            "-E",
-            entry_point,
-            "-Fo",
-            dest_path.to_str().unwrap(),
-            "-Od",
-            "-Zi",
-            "-Qembed_debug"
-        ])
-        .output()
-        .expect("Failed to run dxc");
-
-    stdout().write_all(&result.stdout).unwrap();
-    stderr().write_all(&result.stderr).unwrap();
-
-    if !result.status.success() {
-        panic!("dxc failed: {:?}", result.status.code());
-    }
+    Path::new(DXC_PATH).to_path_buf()
 }
